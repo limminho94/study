b65fd4487433fd1e6f1b5b083e2ce8dffb79fd07
limminho94 <dla47941343@gmail.com>
2024-09-03 21:20:12 +0900

    240903 TCP/IP 챕터3,4,5,7

--- tcp.ip/hello_server.c
+++ tcp.ip/hello_server.c
@@ -9,36 +9,37 @@ void error_handling(char *message);
 
 int main(int argc, char *argv[])
 {
-	int serv_sock;
-	int clnt_sock;
+	int serv_sock;	// serv_sock 변수 선언
+	int clnt_sock;	// clnt_sock 변수 선언
 
-	struct sockaddr_in serv_addr;
-	struct sockaddr_in clnt_addr;
+	struct sockaddr_in serv_addr;	// 구조체 sockaddr_in serv_addr 변수 선언
+	struct sockaddr_in clnt_addr;	// 구조체 sockaddr_in clnt_addr 변수 선언
 	socklen_t clnt_addr_size;
 
-	char message[]="Hello World!";
+	char message[]="Hello World!";	
 	
 	if(argc!=2){
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
-	
+	// 서버 소켓 생성
 	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
 	if(serv_sock == -1)
 		error_handling("socket() error");
-	
+	// 주소정보 초기화
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family=AF_INET;
 	serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
 	serv_addr.sin_port=htons(atoi(argv[1]));
-	
+	// 주소정보 할당
 	if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1 )
 		error_handling("bind() error"); 
-	
+	// 연결요청 대기상태
 	if(listen(serv_sock, 5)==-1)
 		error_handling("listen() error");
 	
-	clnt_addr_size=sizeof(clnt_addr);  
+	clnt_addr_size=sizeof(clnt_addr);
+	// 연결요청 허용
 	clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
 	if(clnt_sock==-1)
 		error_handling("accept() error");  
