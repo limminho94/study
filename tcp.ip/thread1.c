5d609a877a72ed286a1f9abeb9864d274c294936
limminho94 <dla47941343@gmail.com>
2024-09-06 21:05:31 +0900

    240906 챕터 18 쓰레드와 조인

--- tcp.ip/thread1.c
+++ tcp.ip/thread1.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <pthread.h>
 void* thread_main(void *arg);
+void* thread_main1(void *arg);
 
 int main(int argc, char *argv[]) 
 {
 	pthread_t t_id;
+	pthread_t t_id1;
 	int thread_param=5;
-	
+	printf("아아:%s\n", argv[0]);
+	pthread_create(&t_id1, NULL, thread_main, (void*)&thread_param);
 	if(pthread_create(&t_id, NULL, thread_main, (void*)&thread_param)!=0)
 	{
 		puts("pthread_create() error");
 		return -1;
 	}; 	
-	sleep(10);  puts("end of main");
+	while(1){
+	sleep(1);  puts("end of main");
+	
+	}
+
+	
 	return 0;
 }
 
@@ -23,6 +31,17 @@ void* thread_main(void *arg)
 	for(i=0; i<cnt; i++)
 	{
 		sleep(1);  puts("running thread");	 
+	}
+	return NULL;
+}
+
+void* thread_main1(void *arg) 
+{
+	int i;
+	int cnt=*((int*)arg);
+	for(i=0; i<cnt; i++)
+	{
+		sleep(1);  puts("running thread1111");	 
 	}
 	return NULL;
 }
